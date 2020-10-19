8cbebef259ce64c2f061ea3835d8b2d1508a93b2
higsch <matthias.stahl87@web.de>
2020-10-19 23:45:52 +0200

    fetch google trends data from wizard api

--- src/inputs/dataPaths.js
+++ src/inputs/dataPaths.js
@@ -2,3 +2,4 @@ export const data = 'https://raw.githubusercontent.com/DFRLab/interference2020-D
 export const spotData = 'https://raw.githubusercontent.com/DFRLab/interference2020-Data/master/meta/timeline_dates.csv';
 export const coronaData = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv';
 export const images = 'https://raw.githubusercontent.com/DFRLab/interference2020-Data/master/images/';
+export const googleTrendsApiPath = 'https://stormy-cove-42135.herokuapp.com/keyword/';
--- src/utils/loadGoogleTrendsData.js
+++ src/utils/loadGoogleTrendsData.js
@@ -1,6 +1,8 @@
+import { googleTrendsApiPath } from '../inputs/dataPaths';
+
 const loadGoogleTrendsData = async (keyword) => {
-  // const stream = await fetch('https://trends.google.de/trends/api/widgetdata/multiline?req={"time":"2019-10-16 2020-10-16","resolution":"WEEK","locale":"de","comparisonItem":[{"geo":{"country":"US"},"complexKeywordsRestriction":{"keyword":[{"type":"BROAD","value":"voter fraud"}]}}],"requestOptions":{"property":"","backend":"IZG","category":0}}&token=APP6_UEAAAAAX4tiy-FycOn0FX0i21qDBePVHcq_PPt8&tz=-120');
-  // return(stream);
+  const response = await fetch(`${googleTrendsApiPath}${encodeURIComponent(keyword)}`);
+  return(response.json());
 };
 
 export default loadGoogleTrendsData;
