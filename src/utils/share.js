1361ee22919db095a95d2c9687d636eb165275f7
higsch <matthias.stahl87@web.de>
2020-12-03 23:35:28 +0100

    switch share url back to public domain

--- src/utils/share.js
+++ src/utils/share.js
@@ -1,5 +1,5 @@
-// export const baseUrl = 'https://interference2020.org';
-export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://interference2020.org';
+// export const baseUrl = 'http://localhost:5000';
 
 export const urlFromFilters = (disinformantNations,
                                platforms,
